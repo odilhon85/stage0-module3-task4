78f6a15d1f5240c3f74539398795c60106234822
odilhon85 <i.o.r@inbox.ru>
2022-07-16 11:51:25 +0500

    Changes done (first attempt)

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        double a=5.0, b=2.0;
+        double result = ((9*a*a-5*a+2+a-7)*((a+b-4*a*b)/2));
+        System.out.println(result);
     }
 }
